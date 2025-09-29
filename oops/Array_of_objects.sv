167e5866ed4cb05210991b37d8cf21a3365ad741
ADITYA S M <smadityasm@gmail.com>
2025-09-29 18:19:25 +0530

    Create Array_of_objects.sv

---
+++ oops/Array_of_objects.sv
@@ -0,0 +1,16 @@
+class transaction;
+  bit[7:0] data;
+  int id;
+endclass
+
+module array_of_objects;
+  initial begin
+  transaction tr[5];
+  foreach(tr[i]) begin
+    tr[i] = new();
+    tr[i].data = i*i;
+    tr[i].id =i+1;
+    $display("tr[%0d].data = %0d,tr[%0d].id = %0d",i,tr[i].data,i,tr[i].id);
+  end
+  end
+endmodule
